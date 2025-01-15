4c7af89b768de70e96445c3c0c9b936c0b69cebd
Jared <jaredgomez0812@gmail.com>
2025-01-15 16:29:20 -0500

    adding databas url and supabase

--- src/server/db/db_utils.ts
+++ src/server/db/db_utils.ts
@@ -1,6 +1,19 @@
+import { createClient } from "@supabase/supabase-js";
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV || "development"}`,
 });
+
+if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  throw new Error("Supabase environment variables are missing!");
+}
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const isProduction = process.env.NODE_ENV === "production";
 
